de3e144719ed26ed4ce0ec4449c5acc8a55e8e25
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-06 07:31:42 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -32,12 +32,12 @@ class Customer
   
 end
 
-alex = Customer.new("Alex", 30)
-rachel = Customer.new("Rachel", 27)
-dan = Waiter.new("Dan", 3)
+# alex = Customer.new("Alex", 30)
+# rachel = Customer.new("Rachel", 27)
+# dan = Waiter.new("Dan", 3)
  
-rachel.new_meal(dan, 50, 10)
-alex.new_meal(dan, 30, 5)
+# rachel.new_meal(dan, 50, 10)
+# alex.new_meal(dan, 30, 5)
  
 
 
